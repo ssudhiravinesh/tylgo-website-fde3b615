f1e7c90f3787454ef0b1486dd7d251d4d5e94993
Karthik-013 <karthikeyanm.cse2024@citchennai.net>
2025-07-20 18:39:42 +0530

    Update Header.tsx

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -36,7 +36,7 @@ export const Header = ({ user, onLogout, onToggleSidebar }: HeaderProps) => {
             </Button>
 <h1 className="text-xl font-semibold text-gray-800">
   TYL
-  <span style={{ color: "#7851a9", fontWeight: "bold" }}>G</span>
+  <span style={{ color: "orange", fontWeight: "bold" }}>G</span>
   O
 </h1>
 
